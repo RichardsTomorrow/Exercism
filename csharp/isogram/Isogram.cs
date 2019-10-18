e3b4c8b6630d2d57c86c7705ff48f3927d8382e9
Richard <richardwjmorrow@gmail.com>
2019-10-18 15:55:30 -0700

    didnt finish things but will try later

--- csharp/isogram/Isogram.cs
+++ csharp/isogram/Isogram.cs
@@ -1,9 +1,22 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public static class Isogram
 {
     public static bool IsIsogram(string word)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        word = word.ToLower();
+        char[] wordChar = word.ToCharArray().Where(Char.IsLetter);
+
+        Array.Sort(wordChar);
+
+        for (int i = 0; i < word.Length-1; i++)
+        {
+            if (wordChar[i] == wordChar[i + 1])
+                return false;
+        }
+        return true;
+
     }
 }
