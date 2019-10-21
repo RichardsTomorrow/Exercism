d4bf0d0693ddab0a58b113f87564da61cf18ea7b
Richard <richardwjmorrow@gmail.com>
2019-10-21 15:20:24 -0700

    finished perfect numbers challenge

---
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        if (number <= 0)
+        {
+            throw new ArgumentOutOfRangeException("The number entered must be greater than 0.");
+        }
+        int aliquotSum = 0;
+        for (int x = 1; x < number; x++)
+        {
+            if (number % x == 0)
+            {
+                aliquotSum += x;
+            }
+        }
+
+        if (aliquotSum == number)
+        {
+            return Classification.Perfect;
+        }
+        else if (aliquotSum > number)
+        {
+            return Classification.Abundant;
+        }
+        else
+        {
+            return Classification.Deficient;
+        }
+        // throw new NotImplementedException();
+    }
+}
