1cd1bb927eac0ef78c84a4989bfbf074cc73bc6a
Richard <richardwjmorrow@gmail.com>
2019-10-21 13:54:20 -0700

    completed grains

--- csharp/grains/Grains.cs
+++ csharp/grains/Grains.cs
@@ -2,13 +2,23 @@
 
 public static class Grains
 {
-    public static ulong Square(int n)
+    public static ulong Square(int n) //=> (ulong)Math.Pow(2, n - 1);
     {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+        if (n < 1 || n > 64) // this must be first otherwise the square will be calculated first
+        {
+            throw new ArgumentOutOfRangeException("The number must be between 1-64.");
+        }
 
-    public static ulong Total()
-    {
-        throw new NotImplementedException("You need to implement this function.");
+        ulong result = (ulong)Math.Pow(2, n - 1);  // 2^0 = 1 2^1 = 2 2^2 = 4
+        return result;
+    }
+    public static ulong Total() 
+    {       
+        ulong total = 0;
+        for (int i = 1; i <= 64; i++) // i must be 1 or it will be outside of range
+        {
+            total += Square(i);
+        }
+        return total;
     }
 }
