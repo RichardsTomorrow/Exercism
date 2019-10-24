eb7f1e51e80f2bdbdd649d5d1afe736458a35e83
Richard <richardwjmorrow@gmail.com>
2019-10-24 12:41:41 -0700

    updated resistor color answer

--- csharp/resistor-color/ResistorColor.cs
+++ csharp/resistor-color/ResistorColor.cs
@@ -2,53 +2,10 @@
 
 public static class ResistorColor
 {
-    enum resistorColors { black, brown, red, orange, yellow, green, blue, violet, grey, white };
-    
+    private static readonly string[] ColorsArray =
+            {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
 
-    public static int ColorCode(string color)
-    {
-        switch (color)
-        {
-            case "black":
-                return 0;
-                break;
-            case "brown":
-                return 1;
-                break;
-            case "red":
-                return 2;
-                break;
-            case "orange":
-                return 3;
-                break;
-            case "yellow":
-                return 4;
-                break;
-            case "green":
-                return 5;
-                break;
-            case "blue":
-                return 6;
-                break;
-            case "violet":
-                return 7;
-                break;
-            case "grey":
-                return 8;
-                break;
-            case "white":
-                return 9;
-                break;
-            default:
-                return 10;
-                break;
-        }
+    public static int ColorCode(string color) => Array.IndexOf(ColorsArray, color);
 
-    }
-
-    public static string[] Colors()
-    {
-        string[] colours = new string[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
-        return colours;
-    }
+    public static string[] Colors() => ColorsArray;
 }
