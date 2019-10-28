817ff134df25c0b66c24ea65c6bdc94bb549b37e
Richard <richardwjmorrow@gmail.com>
2019-10-28 10:23:33 -0700

    finished Difference of the squares

---
+++ csharp/difference-of-squares/DifferenceOfSquares.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public static class DifferenceOfSquares
+{
+    public static int CalculateSquareOfSum(int max)
+    {
+        int squareOfSum = 0;
+        for (int i = 0; i <= max; i++)
+        {
+            squareOfSum += i;
+        }
+        return squareOfSum * squareOfSum;
+    }
+
+    public static int CalculateSumOfSquares(int max)
+    {
+        int sumOfSquares = 0;
+        for (int j = 0; j <= max; j++)
+        {
+            sumOfSquares += j * j; 
+        }
+        return sumOfSquares;
+    }
+
+    public static int CalculateDifferenceOfSquares(int max)
+    {
+        return CalculateSquareOfSum(max)-CalculateSumOfSquares(max);
+    }
+}
