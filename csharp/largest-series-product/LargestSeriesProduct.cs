c7c005609e6646c01c0a75000a6e217ae55c6b4a
Richard <richardwjmorrow@gmail.com>
2019-10-29 14:52:26 -0700

    finished Largest, downloaded Pythagorean

--- csharp/largest-series-product/LargestSeriesProduct.cs
+++ csharp/largest-series-product/LargestSeriesProduct.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public static class LargestSeriesProduct
 {
     public static long GetLargestProduct(string digits, int span) 
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (span == 0) return 1; 
+        if (span < 0) throw new ArgumentException("The span can't be negative");
+        if (Regex.IsMatch(digits, @"[^0-9]")) throw new ArgumentException("The series of digits must contain numbers");
+        if (span > digits.Length) throw new ArgumentException("The span must be smaller than the length of digits");
+
+
+        long maxProduct = 0;
+        for (int i = 0; i <= digits.Length-span; i++)
+        {
+            long tempProduct = 1;
+
+            for (int j = 0; j < span ; j++)
+            {
+                tempProduct *= Int64.Parse(digits[i + j].ToString());
+
+            }
+                if (tempProduct > maxProduct) { maxProduct = tempProduct; }
+        }
+        return maxProduct;
     }
 }
