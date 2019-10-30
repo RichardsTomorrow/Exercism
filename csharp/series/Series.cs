ae6d8bd18cfe31d086e2f3519016f33cb0cb1b30
Richard <richardwjmorrow@gmail.com>
2019-10-30 10:46:49 -0700

    finished series!

--- csharp/series/Series.cs
+++ csharp/series/Series.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class Series
 {
@@ -6,6 +9,16 @@ public static class Series
     {
         if (sliceLength < 1) throw new ArgumentException("Your slice length must be 1 or greater.");
         if (sliceLength > numbers.Length) throw new ArgumentException("Your slice length must be smaller than the length of the string.");
-        if () //put in a regx for anything but numbers
+        if (Regex.IsMatch(numbers, @"[^\d]")) throw new ArgumentException("Your string must contain only digits");
+
+        List<string> answers = new List<string>();
+        
+        for (int i = 0; i <= numbers.Length - sliceLength; i++)
+        {
+            string sub = numbers.Substring(i, sliceLength);
+            answers.Add(sub);
+        }
+        return answers.ToArray();
+
     }
 }
