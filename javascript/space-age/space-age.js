e023d2ac6afae9c2f48c8d5244d77171d74f2818
Richard <richardwjmorrow@gmail.com>
2019-11-13 15:01:29 -0800

    someone was nice on the internet

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -5,25 +5,25 @@
 
 export const age = (planet, seconds) => {
 
-  let OnEarth = (seconds) =>{
-    return seconds/31557600;
-  }
-if ( planet == 'earth'){
-  return Number(OnEarth(seconds).toFixed(2));
-} else if ( planet == 'mercury'){
-  return Number((OnEarth(seconds)/ 0.2408467).toFixed(2));
-}else if ( planet == 'venus'){
-  return Number((OnEarth(seconds)/ 0.61519726).toFixed(2));
-}else if ( planet == 'mars'){
-  return Number((OnEarth(seconds)/ 1.8808158).toFixed(2));
-}else if ( planet == 'jupiter'){
-  return Number((OnEarth(seconds)/ 11.862615).toFixed(2));
-}else if ( planet == 'saturn'){
-  return Number((OnEarth(seconds)/ 29.447498).toFixed(2));
-}else if ( planet == 'uranus'){
-  return Number((OnEarth(seconds)/ 84.016846).toFixed(2));
-}else if ( planet == 'neptune'){
-  return Number((OnEarth(seconds)/ 164.79132).toFixed(2));
+let OnEarth = (seconds) =>{
+  return seconds/31557600;
 }
-  
+switch(planet){
+  case 'earth':
+    return Number(OnEarth(seconds).toFixed(2));
+  case 'mercury':
+    return Number((OnEarth(seconds)/ 0.2408467).toFixed(2));
+  case 'venus':
+    return Number((OnEarth(seconds)/ 0.61519726).toFixed(2));
+  case 'mars':
+    return Number((OnEarth(seconds)/ 1.8808158).toFixed(2));
+  case 'jupiter':
+    return Number((OnEarth(seconds)/ 11.862615).toFixed(2));
+  case 'saturn':
+    return Number((OnEarth(seconds)/ 29.447498).toFixed(2));
+  case 'uranus':
+    return Number((OnEarth(seconds)/ 84.016846).toFixed(2));
+  case 'neptune':
+    return Number((OnEarth(seconds)/ 164.79132).toFixed(2));
+  }
 };
