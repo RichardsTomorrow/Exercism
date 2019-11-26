6d4a3809ed0c9a873dbf38468ed0d623fe674e7e
Richard <richardwjmorrow@gmail.com>
2019-11-26 09:58:00 -0800

    made some changes

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -1,24 +1,16 @@
-//
-// This is only a SKELETON file for the 'Triangle' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class Triangle {
-  constructor(x, y, z) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
+  constructor(...sides) {
+    this.sides = sides.sort((a,b)=> a-b); // sorts small to large
   }
-
+  
   kind() {
-    if(this.x === 0 || this.y === 0 || this.z === 0){throw "A triangle's sides must have a length greater than 0.";}
-    else if (this.x < 0 || this.y < 0 || this.z < 0){throw "A triangle's side can't be negative.";}
-    else if(!(this.x + this.y > this.z) ||!(this.y + this.z > this.x) ||!(this.z + this.x > this.y) ){
-      throw "This triangle violates triangle equality.";}
-    else if((this.x + this.y === this.z) ||(this.y + this.z === this.x) ||(this.z + this.x === this.y) ){
-      throw "This triangle is a degenerate.";}
-    else if (this.x === this.y && this.y === this.z){return 'equilateral';}
-    else if (this.x === this.y || this.x === this.z || this.y === this.z){return 'isosceles';}
-    else{return 'scalene';}
+    const [x,y,z] = this.sides; //assign variables
+    if(x === 0){throw "A triangle's sides must have a length greater than 0.";} // tests is smallest side is 0
+    else if (x < 0){throw "A triangle's side can't be negative.";} // tests if smallest side is negative
+    else if(!(x + y > z)){throw "This triangle violates triangle equality.";} //tests if triangle meets equality
+    else if((x + y === z)){throw "This triangle is a degenerate.";}// tests if the triangle looks like a straight line
+    else if (x === y && y === z){return 'equilateral';}// matches all side
+    else if (x === y || y === z){return 'isosceles';}// matches one set of sides
+    else{return 'scalene';} // all others are scalene
   }
 }
